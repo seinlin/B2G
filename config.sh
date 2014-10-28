7b6942400993dc76936ed4cde78c8f88d3bd18c7
Kai-Zhen Li <kli@mozilla.com>
2014-10-28 10:21:57 +0800

    Update default REPO and branch.

--- config.sh
+++ config.sh
@@ -30,8 +30,8 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
-BRANCH=${BRANCH:-master}
+GITREPO=${GITREPO:-"git://github.com/Seinlin/b2g-manifest"}
+BRANCH=${BRANCH:-l-preview}
 
 while [ $# -ge 1 ]; do
 	case $1 in
