e17536da8ffc2fb2c2a4801d97f66ec53d712cbe
Fabrice Desré <fabrice@desre.org>
2020-08-06 17:16:39 -0700

    Simplifies sargo configuration
    We also need https://github.com/b2gOS/manifests/pull/4 to align the kernel tag with the blobs version

---
+++ download_sargo_vendor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Download the vendor blobs from https://developers.google.com/android/drivers#sargo
+
+FOUND=0
+
+download() {
+if [ -f "$1" ]
+then
+	echo "$1 already downloaded."
+	FOUND=$(($FOUND + 1))
+else
+	wget https://dl.google.com/dl/android/aosp/$1
+fi
+}
+
+download google_devices-sargo-qq3a.200805.001-ca5e20a1.tgz
+download qcom-sargo-qq3a.200805.001-ebd290fb.tgz
+
+if [ $FOUND != 2 ]
+then
+  echo "About to extract files."
+  # Cleanup vendor/
+  rm -rf vendor/
+  # Extract the scripts.
+  tar xzf google_devices-sargo-qq3a.200805.001-ca5e20a1.tgz
+  tar xzf qcom-sargo-qq3a.200805.001-ebd290fb.tgz
+  ./extract-google_devices-sargo.sh
+  ./extract-qcom-sargo.sh
+fi
+
+echo "Done."
+
